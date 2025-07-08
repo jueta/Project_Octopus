bdf1825f232f3c0a0be8eebe7e6505cecd9fdf4c
jueta <jpmm.brasil@hotmail.com>
2025-07-08 18:08:17 +0200

    PIR + pic

--- src/main.cpp
+++ src/main.cpp
@@ -1,116 +1,88 @@
-
 #include <camera.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include "esp_camera.h"
+#include "esp_sleep.h"
 
-
-// Replace with your network credentials
+// Replace with your WiFi and bot credentials
 const char *ssid = "Casassanta2";
 const char *password = "becky123";
-
-// Use @myidbot to find out the chat ID of an individual or a group
-// Also note that you need to click "start" on a bot before it can
-// message you
 String chatId = "7013307530";
-
-// Initialize Telegram BOT
 String BOTtoken = "7444230490:AAH6sSvrSa69PozjPk_JgiOw_o0Fu9r02-c";
 
-
 WiFiClientSecure clientTCP;
-
 UniversalTelegramBot bot(BOTtoken, clientTCP);
 
+#define PIR_GPIO GPIO_NUM_2 // D1 on XIAO ESP32S3
 
-bool flashState = LOW;
+void blink_led(int times, int delay_ms = 200)
+{
+  for (int i = 0; i < times; i++)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(delay_ms);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(delay_ms);
+  }
+}
 
-// Motion Sensor
-bool motionDetected = false;
-bool sendPhoto = false; // start taking picture
+void setup()
+{
 
-
-int botRequestDelay = 1000;   // mean time between scan messages
-long lastTimeBotRan;     // last time messages' scan has been done
-
-
-
-
-// // Indicates when motion is detected
-// static void IRAM_ATTR detectsMovement(void * arg){
-//   //Serial.println("MOTION DETECTED!!!");
-//   motionDetected = true;
-// }
-
-void setup(){
-
-  // WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
   Serial.begin(115200);
-  
-  // pinMode(FLASH_LED_PIN, OUTPUT);
-  // digitalWrite(FLASH_LED_PIN, flashState);
+  pinMode(PIR_GPIO, INPUT_PULLDOWN); // Most PIRs idle LOW, go HIGH on motion
+  pinMode(LED_BUILTIN, OUTPUT);
+  delay(1000);
 
-  // Config and init the camera
-  configInitCamera();
-  
-  WiFi.mode(WIFI_STA);
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  clientTCP.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
+  // Only run after wakeup from PIR
+  // if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0)
+  // {
+    // Serial.println("Woke up from PIR motion!");
+
+    // Initialize camera
+    configInitCamera();
+
+    // Connect to WiFi
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    clientTCP.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(500);
+      Serial.print(".");
+    }
+
+    blink_led(5); // Blink onboard LED 5 times before sleep
+
+    Serial.println("");
+    Serial.println("WiFi connected!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    // Send photo
+    bot.sendMessage(chatId, "Motion detected! Sending photo...", "");
+    sendPhotoTelegram(&clientTCP, chatId, BOTtoken);
+  // }
+
+  blink_led(5, 100); // Blink onboard LED 5 times before sleep
+
+  // Wait for PIR to go LOW before sleeping
+  while (digitalRead(PIR_GPIO) == HIGH)
+  {
+    Serial.println("Waiting for PIR to go LOW before sleep...");
     delay(500);
   }
-  Serial.println();
-  Serial.print("ESP32-CAM IP Address: ");
-  Serial.println(WiFi.localIP());
 
+  // EXT1: Wake on HIGH signal from PIR
+  esp_sleep_enable_ext1_wakeup(1ULL << PIR_GPIO, ESP_EXT1_WAKEUP_ANY_HIGH);
 
-
-  // // PIR Motion Sensor mode INPUT_PULLUP
-  // // err = gpio_install_isr_service(0); 
-  // err = gpio_isr_handler_add(GPIO_NUM_13, &detectsMovement, (void *) 13);  
-  // if (err != ESP_OK){
-  //   Serial.printf("handler add failed with error 0x%x \r\n", err); 
-  // }
-  // err = gpio_set_intr_type(GPIO_NUM_13, GPIO_INTR_POSEDGE);
-  // if (err != ESP_OK){
-  //   Serial.printf("set intr type failed with error 0x%x \r\n", err);
-  // }
-
-  // Supposely blink flash
-  // for(int i = 0; i < 5; i++){
-  //   digitalWrite(FLASH_LED_PIN, i % 2);
-  //   delay(500);
-  // }
-
+  Serial.println("Sleeping now...");
+  delay(100);
+  esp_deep_sleep_start();
 }
 
-void loop(){
-  if (sendPhoto == true){
-    Serial.println("Preparing photo");
-    sendPhotoTelegram(&clientTCP, chatId, BOTtoken); 
-    sendPhoto = false; 
-  }
-
-  if(motionDetected == true){
-    bot.sendMessage(chatId, "Motion detected!!", "");
-    Serial.println("Motion Detected");
-    sendPhotoTelegram(&clientTCP, chatId, BOTtoken);
-    motionDetected = false;
-  }
-  
-  if (millis() > lastTimeBotRan + botRequestDelay){
-    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-    while (numNewMessages){
-      Serial.println("got response");
-      handleNewMessages(numNewMessages, &sendPhoto, &clientTCP, chatId, BOTtoken, &bot, &flashState);
-      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-    }
-    lastTimeBotRan = millis();
-  }
-
-  delay(1000); // allow time for monitor to connect
-  Serial.println("Hello from XIAO ESP32S3!");
+void loop()
+{
+  // Nothing here – deep sleep handles everything
 }
-
-
